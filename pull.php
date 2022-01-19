7bd4cc152ff5ea53d225a6d2472690f2a91d93da
ceads08 <98046882+ceads08@users.noreply.github.com>
2022-01-19 15:05:52 -0300

    Update pull.php
    proponiendo cambios buble

--- pull.php
+++ pull.php
@@ -9,5 +9,14 @@ $persona = [
            foreach ($persona as $valor) {
             echo "$valor <br>";
            }
+
+$auto = [
+            "Marca" => "Ford",
+            "Modelo" => "Focus",
+            "Año" => 2017
+           ];
            
+           foreach ($auto as $posicion => $valor) {
+            echo "$posicion: $valor <br>";
+           }
 ?>
